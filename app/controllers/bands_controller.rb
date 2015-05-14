d7f8a4567f846fe7b5ecd6a483f6a2dc1734a27a
App Academy Student <student@appacademy.io>
2015-05-14 13:55:25 -0700

    Add right controller to commit

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,53 @@
+class BandsController < ApplicationController
+
+  def index
+    @bands = Band.all
+  end
+
+  def show
+    @band = Band.find(params[:id])
+  end
+
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      flash[:success] = "You have created your cool new band."
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @band = Band.find(params[:id])
+  end
+
+  def update
+    @band = Band.find(params[:id])
+    if @band.update(band_params)
+      flash[:success] = "Your cool band is, hopefully, even cooler."
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @band = Band.find(params[:id])
+    @band.delete
+    flash[:success] = "Your band is so cool it has sublimated into the aether"
+    redirect_to bands_url
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
+end
